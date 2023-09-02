d1f3f5bf2f634b8c3cf14462082d3336a4de40bb
abhyaanand27 <abhyaanand2004@gmail.com>
2023-09-02 23:06:16 +0530

    new commits

--- API/main.py
+++ API/main.py
@@ -4,9 +4,9 @@ app = FastAPI()
 
 
 #get method to send a get request to our API
-@app.get("/")
-async def root():
-    return {"message": "Hello World! Welcome to my API"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World! Welcome to my API"}
 
 '''
 when we update we need to reload the server ie ctrl+c and then run 'uvicorn main:app' 
@@ -15,21 +15,42 @@ short-cut --> 'uvicorn main:app --reload'
 
 # new path operation - represents retrieving social media posts
 
-@app.get("/input_get")
-def get_posts():
-    return {"message":"will return post_input"}
+# @app.get("/input_get")
+# def get_posts():
+#     return {"message":"will return post_input"}
 
-@app.get("/output_get")
-def get_posts():
-    return {"message":"will return post_output"}
+# @app.get("/output_get")
+# def get_posts():
+#     return {"message":"will return post_output"}
 
-@app.post("/input")
-def create_post():
-    return {"message":"user input (movie name)"}
-@app.post("/output")
-def create_post():
-    return {"message":"review"}
+# @app.post("/input")
+# def create_post():
+#     return {"message":"user input (movie name)"}
+# @app.post("/output")
+# def create_post():
+#     return {"message":"review"}
 
-@app.put("/update")
-def update_post():
-    return{}
+# @app.put("/update")
+# def update_post():
+#     return{}
+
+@app.get("/")
+def index():
+    return {'data':'blog list'}
+
+#list of all unpublished blogs
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data":"all unpublised blogs"}
+
+@app.get("/blog/{blog_id}")
+def show(blog_id:int):  #(only integer value is allowed)--> error if not a int 
+    #fetch blog with id = blog_id
+    return {'data':blog_id}
+
+
+
+@app.get("/blog/{blog_id}/comments")
+def comments(blog_id):
+    #fetch comments of blog with id = blog_id
+    return {'data':{'1','2'}}
