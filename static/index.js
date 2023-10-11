8a934f5a93760489e3abab22e3110658bbbfdba5
Aditya Paul <122125787+Paulie-Aditya@users.noreply.github.com>
2023-10-11 12:29:34 +0530

    Update index.js

--- static/index.js
+++ static/index.js
@@ -14,29 +14,52 @@ function submitMovie(){
 
     .then(response => response.json())
     .then(data => {
-        var s = `
+          var s = `
+          <h2 align="center">Movie: ${data.title}</h2>
 
-        <table border="2">
-        <thead>
-          <h2 align="center">${data.title} Analysis</h2>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Movie Name</td>
-            <td>${data.title}</td>
-          </tr>
-          <tr>
-            <td>Released date</td>
-            <td>${data.release_year}</td>
-          </tr>
-          <tr>
-            <td>Poster</td>
-            <td>
-              <img src=${data.poster_url} alt=${data.title} />
-            </td>
-          </tr>
-        </tbody>
-      </table>`;
+          <table border="2">
+          <tbody>
+            <tr>
+              <td>Movie Name</td>
+              <td>${data.title}</td>
+            </tr>
+            <tr>
+              <td>Sentiment</td>
+              <td>${data.sentiment} ${data.percentage}</td>
+            </tr>
+            <tr>
+              <td>Released date</td>
+              <td>${data.release_year}</td>
+            </tr>
+            <tr>
+              <td>Genre</td>
+              <td>${data.genre}</td>
+            </tr>
+            <tr>
+              <td>Actors</td>
+              <td>${data.cast}</td>
+            </tr>
+            <tr>
+              <td>Director</td>
+              <td>${data.director}</td>
+            </tr>
+            <tr>
+              <td>Plot</td>
+              <td>${data.plot}</td>
+            </tr>
+            <tr>
+              <td>Poster</td>
+              <td>
+                <img src=${data.poster_url} alt=${data.title} />
+              </td>
+            </tr>
+            <tr>
+              <td>IMDb Rating</td>
+              <td>${data.imdbRating}</td>
+            </tr>
+          </tbody>
+        </table>
+        `;
       document.getElementById("container").innerHTML = s;
       })
     /*
